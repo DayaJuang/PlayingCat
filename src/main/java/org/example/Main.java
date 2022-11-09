ebdfef367261ee8c73b9e80ddfb9b8110c476ad6
daya <dayajuangmahaputra@gmail.com>
2022-11-09 11:00:24 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean ans = isCatPlaying(false,36);
+
+        System.out.println(ans);
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        // Initiate the min and max temperature cat playing
+        int minTemp = 25;
+        int maxTemp = 35;
+
+        // if its summer change the max temp to 45
+        if(summer)
+            maxTemp = 45;
+
+        // Check if cat playing or not
+        return (temperature >= minTemp) && (temperature <= maxTemp);
     }
 }
